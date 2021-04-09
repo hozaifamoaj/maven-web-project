a75fe0d77987143e4c3d312e21206d1e81dc2d62
hozaifa <hozaifa@istlbd.com>
2021-04-09 20:43:35 +0600

    add all file to git

---
+++ src/main/java/com/test/demomavenjen/DemomavenjenApplication.java
@@ -0,0 +1,13 @@
+package com.test.demomavenjen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomavenjenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomavenjenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/demomavenjen/DemomavenjenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.demomavenjen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomavenjenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
